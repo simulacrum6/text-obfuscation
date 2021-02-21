5e6e6884092de3ee57af12f2d544a961b44ed439
Marius Hamacher <marius.hamacher@outlook.de>
2021-02-21 16:27:23 +0100

    add combining characters. add composite generator.

--- dist/obfuscation.js
+++ dist/obfuscation.js
@@ -1,6 +1,17 @@
 "use strict";
-function randCheck(target) {
-    return Math.random() < target;
+function randCheck(probability) {
+    return Math.random() < probability;
+}
+function randChoice(a, probs) {
+    if (!probs) {
+        probs = Array(a.length).fill(1 / a.length);
+    }
+    const t = Math.random();
+    const i = probs.sort(diff)
+        .map(cumSum())
+        .map(p => t < p)
+        .indexOf(true);
+    return a[i];
 }
 function randInt(min = 0, max = 100) {
     return Math.floor(Math.random() * (max + 1 - min)) + min;
@@ -12,6 +23,15 @@ function zip(a, b) {
         c[i] = [a[i % a.length], b[i % b.length]];
     }
     return c;
+}
+function sum(a, b) {
+    return a + b;
+}
+function diff(a, b) {
+    return a - b;
+}
+function cumSum(sum = 0) {
+    return (value) => sum += value;
 }
 class Obfuscator {
     constructor() {
@@ -40,8 +60,19 @@ class CodePointInsertionObfuscator extends Obfuscator {
         this.obfuscationProbability = obfuscationProbability;
     }
     static CombiningDiacriticalMarks(minInsertions, maxInsertions, obfuscationProbability) {
-        const cpg = CodePointGenerator.CombiningDiacriticalMarks();
+        const cpg = RangeCodePointGenerator.CombiningDiacriticalMarks();
         return new CodePointInsertionObfuscator(cpg, minInsertions, maxInsertions, obfuscationProbability);
+    }
+    static CombiningCharacters(minInsertions, maxInsertions, obfuscationProbability) {
+        const cpgs = [
+            RangeCodePointGenerator.CombiningDiacriticalMarks(),
+            RangeCodePointGenerator.CombiningDiacriticalMarksExtended(),
+            RangeCodePointGenerator.CombiningDiacriticalMarksSupplement(),
+            RangeCodePointGenerator.CombiningDiacriticalMarksForSymbols(),
+            RangeCodePointGenerator.CombiningHalfMarks()
+        ];
+        const cpg = new CombinedCodePointGenerator(cpgs);
+        return new CodePointInsertionObfuscator(cpg);
     }
     obfuscateChar(char) {
         const numInsertions = randInt(this.minInsertions, this.maxInsertions);
@@ -52,21 +83,56 @@ CodePointInsertionObfuscator.MIN_INSERTIONS = 0;
 CodePointInsertionObfuscator.MAX_INSERTIONS = 0;
 CodePointInsertionObfuscator.OBFUSCATION_PROBABILITY = 0.25;
 class CodePointGenerator {
-    constructor(minCodePoint, maxCodePoint) {
-        this.minCodePoint = minCodePoint;
-        this.maxCodePoint = maxCodePoint;
-    }
-    static CombiningDiacriticalMarks() {
-        return new CodePointGenerator(0x0300, 0x036F);
-    }
-    generateCodePoint() {
-        return randInt(this.minCodePoint, this.maxCodePoint);
-    }
     generateChar() {
         return String.fromCodePoint(this.generateCodePoint());
     }
     generateString(length) {
         return Array(length).fill(null).map(_ => this.generateChar()).join('');
+    }
+}
+class RangeCodePointGenerator extends CodePointGenerator {
+    constructor(minCodePoint, maxCodePoint) {
+        super();
+        this.minCodePoint = minCodePoint;
+        this.maxCodePoint = maxCodePoint;
+    }
+    static CombiningDiacriticalMarks() {
+        return new RangeCodePointGenerator(0x0300, 0x036F);
+    }
+    static CombiningDiacriticalMarksExtended() {
+        return new RangeCodePointGenerator(0x1AB0, 0x1AC0);
+    }
+    static CombiningDiacriticalMarksSupplement() {
+        return new RangeCodePointGenerator(0x1DC0, 0x1DFF);
+    }
+    static CombiningDiacriticalMarksForSymbols() {
+        return new RangeCodePointGenerator(0x20D0, 0x20F0);
+    }
+    static CombiningHalfMarks() {
+        return new RangeCodePointGenerator(0xFE20, 0xFE2F);
+    }
+    getNumberOfCodePoints() {
+        return this.maxCodePoint - this.minCodePoint;
+    }
+    generateCodePoint() {
+        return randInt(this.minCodePoint, this.maxCodePoint);
+    }
+}
+class CombinedCodePointGenerator extends CodePointGenerator {
+    constructor(cpgs, probs) {
+        super();
+        this.cpgs = cpgs;
+        this.probs = probs;
+        if (!probs) {
+            const total = this.getNumberOfCodePoints();
+            this.probs = cpgs.map(cpg => cpg.getNumberOfCodePoints()).map(n => n / total);
+        }
+    }
+    getNumberOfCodePoints() {
+        return this.cpgs.map(cpg => cpg.getNumberOfCodePoints()).reduce(sum);
+    }
+    generateCodePoint() {
+        return randChoice(this.cpgs).generateCodePoint();
     }
 }
 function obfuscateInput() {
--- src/obfuscation.ts
+++ src/obfuscation.ts
@@ -1,5 +1,18 @@
-function randCheck(target: number) {
-    return Math.random() < target
+function randCheck(probability: number) {
+    return Math.random() < probability
+}
+
+function randChoice<T>(a: T[], probs?: number[]) {
+    if (!probs) {
+        probs = Array(a.length).fill(1 / a.length)
+    }
+    
+    const t = Math.random()
+    const i = probs.sort(diff)
+        .map(cumSum())
+        .map(p => t < p)
+        .indexOf(true) 
+    return a[i]
 }
 
 function randInt(min = 0, max = 100) {
@@ -13,6 +26,18 @@ function zip<T, S>(a: T[], b: S[]) {
         c[i] = [a[i % a.length], b[i % b.length]]
     }
     return c
+}
+
+function sum(a: number, b: number) {
+    return a + b
+}
+
+function diff(a: number, b: number) {
+    return a - b
+}
+
+function cumSum(sum = 0) {
+    return (value: number) => sum += value
 }
 
 abstract class Obfuscator {
@@ -43,8 +68,20 @@ class CodePointInsertionObfuscator extends Obfuscator {
     static OBFUSCATION_PROBABILITY = 0.25 
 
     static CombiningDiacriticalMarks(minInsertions?: number, maxInsertions?: number, obfuscationProbability?: number) {
-        const cpg = CodePointGenerator.CombiningDiacriticalMarks()
+        const cpg = RangeCodePointGenerator.CombiningDiacriticalMarks()
         return new CodePointInsertionObfuscator(cpg, minInsertions, maxInsertions, obfuscationProbability)
+    }
+
+    static CombiningCharacters(minInsertions?: number, maxInsertions?: number, obfuscationProbability?: number) {
+        const cpgs = [
+            RangeCodePointGenerator.CombiningDiacriticalMarks(),
+            RangeCodePointGenerator.CombiningDiacriticalMarksExtended(),
+            RangeCodePointGenerator.CombiningDiacriticalMarksSupplement(),
+            RangeCodePointGenerator.CombiningDiacriticalMarksForSymbols(),
+            RangeCodePointGenerator.CombiningHalfMarks()
+        ]
+        const cpg: CodePointGenerator = new CombinedCodePointGenerator(cpgs)
+        return new CodePointInsertionObfuscator(cpg)
     }
 
     constructor(public cpg: CodePointGenerator, public minInsertions = 0, public maxInsertions = 3, public obfuscationProbability = 0.25) {
@@ -57,23 +94,68 @@ class CodePointInsertionObfuscator extends Obfuscator {
     }
 }
 
-class CodePointGenerator {
-    static CombiningDiacriticalMarks() {
-        return new CodePointGenerator(0x0300, 0x036F)
-    }
-
-    constructor(public minCodePoint: number, public maxCodePoint: number) { }
-
-    generateCodePoint() {
-        return randInt(this.minCodePoint, this.maxCodePoint)
-    }
-
+abstract class CodePointGenerator {
+    abstract getNumberOfCodePoints(): number
+    abstract generateCodePoint(): number
+    
     generateChar() {
         return String.fromCodePoint(this.generateCodePoint())
     }
 
     generateString(length: number) {
         return Array(length).fill(null).map(_ => this.generateChar()).join('')
+    }
+}
+
+class RangeCodePointGenerator extends CodePointGenerator {
+    static CombiningDiacriticalMarks() {
+        return new RangeCodePointGenerator(0x0300, 0x036F)
+    }
+
+    static CombiningDiacriticalMarksExtended() {
+        return new RangeCodePointGenerator(0x1AB0, 0x1AC0)
+    }
+
+    static CombiningDiacriticalMarksSupplement() {
+        return new RangeCodePointGenerator(0x1DC0, 0x1DFF)
+    }
+
+    static CombiningDiacriticalMarksForSymbols() {
+        return new RangeCodePointGenerator(0x20D0, 0x20F0)
+    }
+
+    static CombiningHalfMarks() {
+        return new RangeCodePointGenerator(0xFE20, 0xFE2F)
+    }
+
+    constructor(public minCodePoint: number, public maxCodePoint: number) { super() }
+
+    getNumberOfCodePoints() {
+        return this.maxCodePoint - this.minCodePoint
+    }
+
+    generateCodePoint() {
+        return randInt(this.minCodePoint, this.maxCodePoint)
+    }
+}
+
+class CombinedCodePointGenerator extends CodePointGenerator {
+    
+    constructor(private cpgs: CodePointGenerator[], private probs?: number[]) {
+        super()
+
+        if (!probs) {
+            const total = this.getNumberOfCodePoints()
+            this.probs = cpgs.map(cpg => cpg.getNumberOfCodePoints()).map(n => n / total) 
+        }
+    }
+    
+    getNumberOfCodePoints(): number {
+        return this.cpgs.map(cpg => cpg.getNumberOfCodePoints()).reduce(sum)
+    }
+
+    generateCodePoint(): number {
+        return randChoice(this.cpgs).generateCodePoint()
     }
 }
 
